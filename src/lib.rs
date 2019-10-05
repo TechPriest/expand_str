085433b6f5928924e4e9976a483558b72c332bb0
Vladimir Yarovenko <vladimiryarovenko@Vladimirs-Mini.local>
2019-10-05 18:01:51 +0300

    Working on ExpandableStringSplit

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,11 @@
+use std::iter::Iterator;
+
 #[derive(Debug)]
 pub struct ExpandableStringSplit<'a> {
     src: &'a str,
     chars_iter: std::str::CharIndices<'a>,
     token_start: usize,
+    reading_var: bool,
 }
 
 impl<'a> ExpandableStringSplit<'a> {
@@ -11,7 +14,32 @@ impl<'a> ExpandableStringSplit<'a> {
             chars_iter: src.char_indices(),
             src,
             token_start: 0,
+            reading_var: false,
         }
+    }
+}
+
+#[derive(Debug)]
+pub enum ExpandableStrEntry<'a> {
+    Substr(&'a str),
+    Var(&'a str),
+}
+
+impl<'a> Iterator for ExpandableStringSplit<'a> {
+    type Item = &'a str;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let mut token_len = 0usize;
+        while let Some((n, c)) = self.chars_iter.next() {
+            if c == '%' {
+
+                self.reading_var != self.reading_var;
+            }
+
+            token_len += 1;
+        }
+
+        unimplemented!()
     }
 }
 
