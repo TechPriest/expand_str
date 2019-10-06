fdfc0141dd3a1d9159d2d570893e60fefc5ff64b
TechPriest <yaminogakusei@gmail.com>
2019-10-06 18:28:11 +0300

    Working on ExpandableStringSplit

--- src/lib.rs
+++ src/lib.rs
@@ -26,20 +26,26 @@ pub enum ExpandableStrEntry<'a> {
 }
 
 impl<'a> Iterator for ExpandableStringSplit<'a> {
-    type Item = &'a str;
+    type Item = ExpandableStrEntry<'a>;
 
     fn next(&mut self) -> Option<Self::Item> {
-        let mut token_len = 0usize;
         while let Some((n, c)) = self.chars_iter.next() {
             if c == '%' {
-
-                self.reading_var != self.reading_var;
+                let reading_var = self.reading_var;
+                self.reading_var = !reading_var;
+                if n > 0 {
+                    let token_slice = &self.src[self.token_start .. n];
+                    self.token_start = n + 1;
+                    if reading_var {
+                        return Some(ExpandableStrEntry::Var(token_slice));
+                    } else {
+                        return Some(ExpandableStrEntry::Substr(token_slice));
+                    }
+                }
             }
-
-            token_len += 1;
         }
 
-        unimplemented!()
+        None
     }
 }
 
